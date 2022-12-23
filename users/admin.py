4c1468458ebbef899dabca8c5b79cabc984a82a7
Amirreza Taherkhani <ar.taher404@gmail.com>
2022-12-23 16:50:37 +0330

    remove unnecessary imports

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from .models import User, Profile
+from .models import Profile
 
 
 # ordering is commented on UserAdmin
